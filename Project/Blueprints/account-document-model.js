7081862b5a4ae86fdf2d02c334cd562da9c5b106
ImbayaDennis <43815591+ImbayaDennis@users.noreply.github.com>
2020-09-29 23:12:21 +0300

    Setting  up
    Adding all ccurrently gathered resources to the repository including Project wireframes and model document blueprint

---
+++ Project/Blueprints/account-document-model.js
@@ -0,0 +1,35 @@
+const account ={
+    username: "",
+    firstname: "",
+    lastname: "",
+    password: "",
+    email: "",
+    data: {
+        modules:[
+            {
+                name: "",
+                facilitator: "",
+                start: "",
+                end: "",
+                projects: [
+                    {
+                        name: "",
+                        start: "",
+                        due: "",
+                        milestones: [
+                            {
+                                name: "",
+                                description: "",
+                                start: "",
+                                end: ""
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        preferences: [
+            
+        ]
+    }
+}
